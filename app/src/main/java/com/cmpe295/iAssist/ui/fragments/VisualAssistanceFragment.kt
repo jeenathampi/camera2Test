4bbb92e5d590829fe9b4fb1903b9618913b4dbb5
Jeena <jeenathampi.23@gmail.com>
2021-10-14 14:42:55 -0700

    added camera2 api

--- app/src/main/java/com/cmpe295/iAssist/ui/fragments/VisualAssistanceFragment.kt
+++ app/src/main/java/com/cmpe295/iAssist/ui/fragments/VisualAssistanceFragment.kt
@@ -1,7 +1,6 @@
 package com.cmpe295.iAssist.ui.fragments
 
-import android.annotation.SuppressLint
-import android.content.Context
+import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -23,12 +22,9 @@ import android.view.*
 import android.view.TextureView.SurfaceTextureListener
 import android.widget.Button
 import android.widget.Toast
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
-import com.cmpe295.iAssist.Constants
 import com.cmpe295.iAssist.databinding.FragmentVisualBinding
 import com.google.gson.Gson
 import edmt.dev.edmtdevcognitivevision.Contract.AnalysisResult
@@ -42,6 +38,7 @@ import kotlinx.coroutines.withContext
 import java.io.*
 import java.util.*
 
+
 class VisualAssistanceFragment : Fragment() {
 
     var bitmap: Bitmap? = null
@@ -49,8 +46,8 @@ class VisualAssistanceFragment : Fragment() {
     lateinit var res : AnalysisResult
     lateinit var visionServiceClient : VisionServiceClient
     companion object {
-        val API_KEY = "*********"
-        val API_LINK = "************"
+        val API_KEY = "*******"
+        val API_LINK = "*********"
         val ORIENTATIONS = SparseIntArray()
         init {
             ORIENTATIONS.append(Surface.ROTATION_0, 90)
@@ -60,23 +57,17 @@ class VisualAssistanceFragment : Fragment() {
         }
     }
     private lateinit var binding: FragmentVisualBinding
-    private var imageCapture: ImageCapture?=null
     lateinit var mTextToSpeech: TextToSpeech
 
     //camera2 stuff
     private val TAG: String? = "AndroidCameraApi"
-    private val takePictureButton: Button? = null
-    private val textureView: TextureView? = null
-    private val cameraId: String? = null
+    private var textureView: TextureView? = null
     protected var cameraDevice: CameraDevice? = null
     protected var cameraCaptureSessions: CameraCaptureSession? = null
-    protected var captureRequest: CaptureRequest? = null
     protected var captureRequestBuilder: CaptureRequest.Builder? = null
-    private val imageDimension: Size? = null
-    private val imageReader: ImageReader? = null
-    private val file: File? = null
+    private var imageDimension: Size? = null
+    private var imageReader: ImageReader? = null
     private val REQUEST_CAMERA_PERMISSION = 200
-    private val mFlashSupported = false
     private var mBackgroundHandler: Handler? = null
     private var mBackgroundThread: HandlerThread? = null
 
@@ -103,7 +94,9 @@ class VisualAssistanceFragment : Fragment() {
             takePicture()
         }
 
-        binding.texture.surfaceTextureListener = textureListener
+        textureView = binding.texture
+        assert(textureView != null)
+        textureView!!.surfaceTextureListener = textureListener
 
         mTextToSpeech = TextToSpeech(requireContext(), TextToSpeech.OnInitListener { status ->
             if (status != TextToSpeech.ERROR) {
@@ -203,9 +196,9 @@ class VisualAssistanceFragment : Fragment() {
             Log.e(TAG, "cameraDevice is null")
             return
         }
-        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val manager = getSystemService(requireContext(), CameraManager::class.java)
         try {
-            val characteristics = manager.getCameraCharacteristics(
+            val characteristics = manager?.getCameraCharacteristics(
                 cameraDevice!!.id
             )
             var jpegSizes: Array<Size>? = null
@@ -213,6 +206,7 @@ class VisualAssistanceFragment : Fragment() {
                 jpegSizes =
                     characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
                         .getOutputSizes(ImageFormat.JPEG)
+                Log.e("CameraCharacteristics","${CameraCharacteristics.LENS_INFO_FOCUS_DISTANCE_CALIBRATION}")
             }
             var width = 640
             var height = 480
@@ -229,10 +223,12 @@ class VisualAssistanceFragment : Fragment() {
             captureBuilder.addTarget(reader.surface)
             captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
             // Orientation
-            val rotation: Int = getWindowManager().getDefaultDisplay().getRotation()
+            val display = (getSystemService(requireContext(),WindowManager::class.java))
+                ?.getDefaultDisplay()
+            val rotation: Int = display?.getRotation() ?: 0;
             captureBuilder.set(
                 CaptureRequest.JPEG_ORIENTATION,
-                MainActivity.ORIENTATIONS.get(rotation)
+                ORIENTATIONS.get(rotation)
             )
             val file = File(Environment.getExternalStorageDirectory().toString() + "/pic.jpg")
             val readerListener: ImageReader.OnImageAvailableListener =
@@ -244,7 +240,15 @@ class VisualAssistanceFragment : Fragment() {
                             val buffer = image.planes[0].buffer
                             val bytes = ByteArray(buffer.capacity())
                             buffer[bytes]
-                            save(bytes)
+                            val clonedBytes = bytes.clone()
+                            bitmap = BitmapFactory.decodeByteArray(clonedBytes, 0, clonedBytes.size)
+                    visionServiceClient = VisionServiceRestClient(API_KEY, API_LINK)
+                    val uiScope = CoroutineScope(Dispatchers.Main)
+                    //TODO
+                    uiScope.launch {
+                        processimage()
+                    }
+//                            save(bytes)
                         } catch (e: FileNotFoundException) {
                             e.printStackTrace()
                         } catch (e: IOException) {
@@ -274,7 +278,7 @@ class VisualAssistanceFragment : Fragment() {
                 ) {
                     super.onCaptureCompleted(session, request, result)
                     Log.e(
-                        MainActivity.TAG,
+                        TAG,
                         String.format(
                             "captureCallbackListener %s-%f",
                             result.get(CaptureResult.LENS_STATE).toString(),
@@ -282,19 +286,20 @@ class VisualAssistanceFragment : Fragment() {
                         )
                     )
                     Log.e(
-                        MainActivity.TAG,
+                        TAG,
                         String.format(
                             "AF mode %s-%s",
                             result.get(CaptureResult.CONTROL_AF_MODE).toString(),
                             result.get(CaptureResult.CONTROL_AF_STATE).toString()
                         )
                     )
-                    val calculated_distance = 1 / result.get(CaptureResult.LENS_FOCUS_DISTANCE)!!
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Distance: $calculated_distance meters",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    //uncomment this if not  null
+//                    val calculated_distance = 1 / result.get(CaptureResult.LENS_FOCUS_DISTANCE)!!
+//                    Toast.makeText(
+//                        requireContext(),
+//                        "Distance: $calculated_distance meters",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
                     createCameraPreview()
                 }
             }
@@ -341,10 +346,104 @@ class VisualAssistanceFragment : Fragment() {
         }
     }
 
-    private fun allPermissionGranted() =
-        Constants.REQUIRED_PERMISSIONS.all {
-            context?.let { it1 -> ContextCompat.checkSelfPermission(it1, it) } == PackageManager.PERMISSION_GRANTED
+    protected fun createCameraPreview() {
+        try {
+            val texture = textureView!!.surfaceTexture!!
+            texture.setDefaultBufferSize(imageDimension!!.width, imageDimension!!.height)
+            val surface = Surface(texture)
+            captureRequestBuilder =
+                cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            captureRequestBuilder!!.addTarget(surface)
+            cameraDevice!!.createCaptureSession(
+                Arrays.asList(surface),
+                object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
+                        //The camera is already closed
+                        if (null == cameraDevice) {
+                            return
+                        }
+                        // When the session is ready, we start displaying the preview.
+                        cameraCaptureSessions = cameraCaptureSession
+                        updatePreview()
+                    }
+
+                    override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
+                        Toast.makeText(
+                            requireContext(),
+                            "Configuration change",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                },
+                null
+            )
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
         }
+    }
+
+    private fun openCamera() {
+        val manager = getSystemService(requireContext(),CameraManager::class.java)
+        Log.e(TAG, "is camera open")
+        try {
+            val cameraIds = manager!!.cameraIdList
+            val characteristics = manager!!.getCameraCharacteristics(cameraIds.get(0))
+            val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+            imageDimension = map.getOutputSizes(SurfaceTexture::class.java)[0]
+            // Add permission for camera and let user grant the permission
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    REQUEST_CAMERA_PERMISSION
+                )
+                return
+            }
+            manager.openCamera(cameraIds.get(0), stateCallback, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+        Log.e(TAG, "openCamera X")
+    }
+
+    protected fun updatePreview() {
+        if (null == cameraDevice) {
+            Log.e(TAG, "updatePreview error, return")
+        }
+        captureRequestBuilder!!.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+        try {
+            cameraCaptureSessions!!.setRepeatingRequest(
+                captureRequestBuilder!!.build(),
+                null,
+                mBackgroundHandler
+            )
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun closeCamera() {
+        if (null != cameraDevice) {
+            cameraDevice!!.close()
+            cameraDevice = null
+        }
+        if (null != imageReader) {
+            imageReader!!.close()
+            imageReader = null
+        }
+    }
+
+//    private fun allPermissionGranted() =
+//        Constants.REQUIRED_PERMISSIONS.all {
+//            context?.let { it1 -> ContextCompat.checkSelfPermission(it1, it) } == PackageManager.PERMISSION_GRANTED
+//        }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -352,83 +451,103 @@ class VisualAssistanceFragment : Fragment() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == Constants.REQUEST_CODE_PERMISSIONS){
-            if(allPermissionGranted()){
-                //code
-                startCamera()
-            }else{
-                Toast.makeText(requireActivity(), "Permission not granted by the user", Toast.LENGTH_SHORT).show()
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                // close the app
+                Toast.makeText(
+                    requireContext(),
+                    "Sorry!!!, you can't use this app without granting permission",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
 
-    @SuppressLint("UnsafeExperimentalUsageError")
-    private fun takePhoto(){
-
-        val imageCapture = imageCapture?: return
-        imageCapture.takePicture(
-            ContextCompat.getMainExecutor(requireContext()), object :ImageCapture.OnImageCapturedCallback() {
-                override fun onCaptureSuccess(image: ImageProxy) {
-                    super.onCaptureSuccess(image)
-                    Log.i("assist","captured photo")
-                    // code to call azure api
-                    /**
-                     * Convert Image Proxy to Bitmap
-                     */
-                    Log.i("ImageFormat", "${image.format}")
-                    fun toBitmap(image: ImageProxy): Bitmap? {
-                        val byteBuffer = image.planes[0].buffer
-                        byteBuffer.rewind()
-                        val bytes = ByteArray(byteBuffer.capacity())
-                        byteBuffer[bytes]
-                        val clonedBytes = bytes.clone()
-                        return BitmapFactory.decodeByteArray(clonedBytes, 0, clonedBytes.size)
-                    }
-
-                    bitmap = toBitmap(image)
-                    visionServiceClient = VisionServiceRestClient(VisualAssistanceFragment.API_KEY, VisualAssistanceFragment.API_LINK)
-                    val uiScope = CoroutineScope(Dispatchers.Main)
-                    //TODO
-                    uiScope.launch {
-                        processimage()
-                    }
-                    image.close()
-                }
-
-                override fun onError(exception: ImageCaptureException) {
-                    super.onError(exception)
-                    Log.e(Constants.TAG, "onError: ${exception.message}", exception)
-                }
-            }
-        )
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+        startBackgroundThread()
+        if (textureView!!.isAvailable) {
+            openCamera()
+        } else {
+            textureView!!.surfaceTextureListener = textureListener
+        }
     }
 
-    private fun startCamera(){
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-
-        cameraProviderFuture.addListener(Runnable {
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-            val preview = Preview.Builder()
-                .build()
-                .also { mPreview->
-                    mPreview.setSurfaceProvider(
-                        binding.viewFinder.surfaceProvider
-                    )
-                }
-            imageCapture = ImageCapture.Builder()
-                .build()
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture
-                )
-            }catch (e: Exception){
-                Log.d(Constants.TAG, "StartCamera Fail:", e)
-            }
-        }, ContextCompat.getMainExecutor(requireContext()))
+    override fun onPause() {
+        Log.e(TAG, "onPause")
+        //closeCamera();
+        stopBackgroundThread()
+        super.onPause()
     }
+
+//    @SuppressLint("UnsafeExperimentalUsageError")
+//    private fun takePhoto(){
+//
+//        val imageCapture = imageCapture?: return
+//        imageCapture.takePicture(
+//            ContextCompat.getMainExecutor(requireContext()), object :ImageCapture.OnImageCapturedCallback() {
+//                override fun onCaptureSuccess(image: ImageProxy) {
+//                    super.onCaptureSuccess(image)
+//                    Log.i("assist","captured photo")
+//                    // code to call azure api
+//                    /**
+//                     * Convert Image Proxy to Bitmap
+//                     */
+//                    Log.i("ImageFormat", "${image.format}")
+//                    fun toBitmap(image: ImageProxy): Bitmap? {
+//                        val byteBuffer = image.planes[0].buffer
+//                        byteBuffer.rewind()
+//                        val bytes = ByteArray(byteBuffer.capacity())
+//                        byteBuffer[bytes]
+//                        val clonedBytes = bytes.clone()
+//                        return BitmapFactory.decodeByteArray(clonedBytes, 0, clonedBytes.size)
+//                    }
+//
+//                    bitmap = toBitmap(image)
+//                    visionServiceClient = VisionServiceRestClient(VisualAssistanceFragment.API_KEY, VisualAssistanceFragment.API_LINK)
+//                    val uiScope = CoroutineScope(Dispatchers.Main)
+//                    //TODO
+//                    uiScope.launch {
+//                        processimage()
+//                    }
+//                    image.close()
+//                }
+//
+//                override fun onError(exception: ImageCaptureException) {
+//                    super.onError(exception)
+//                    Log.e(Constants.TAG, "onError: ${exception.message}", exception)
+//                }
+//            }
+//        )
+//    }
+
+//    private fun startCamera(){
+//        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+//
+//        cameraProviderFuture.addListener(Runnable {
+//            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+//            val preview = Preview.Builder()
+//                .build()
+//                .also { mPreview->
+//                    mPreview.setSurfaceProvider(
+//                        binding.viewFinder.surfaceProvider
+//                    )
+//                }
+//            imageCapture = ImageCapture.Builder()
+//                .build()
+//
+//            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+//            try {
+//                cameraProvider.unbindAll()
+//                cameraProvider.bindToLifecycle(
+//                    this, cameraSelector, preview, imageCapture
+//                )
+//            }catch (e: Exception){
+//                Log.d(Constants.TAG, "StartCamera Fail:", e)
+//            }
+//        }, ContextCompat.getMainExecutor(requireContext()))
+//    }
 
     private suspend fun processimage() {
         withContext(Dispatchers.Default) {
